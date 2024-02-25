f90678c91448b6934e1ab764cc39bd6fe3a7ff2f
George Davis <davisgl055@ecc.edu>
2024-02-25 17:44:07 -0500

    Split Iventory.cpp into functions.

--- Item.cpp
+++ Item.cpp
@@ -43,6 +43,121 @@ void OpenInvenentoryFile(ifstream& File)
 		File.open(Filename, ios::in || ios::app);
 	}
 }
+void ChooseOperation(ifstream& Data, Item* Inventory, int& Size, int& Count) 
+{
+	cout << "Input your operation on the inventory: ";
+	string Operation;
+	transform(Operation.begin(), Operation.end(), Operation.begin(), tolower); // Convert Current Itemname to lowercase
+	cin >> Operation;
+	if (Operation == "add item" || Operation == "add")
+	{
+		Item NewItem = DefineItem();
+		int Position = 0;
+		cout << "What position do you want your new object to be in?";
+		do
+		{
+			cin >> Position;
+			if (!cin)
+			{
+				cerr << "Invalid Position must be within 0 and " << Size - 1;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+			}
+		}
+		while (!cin);
+		AddItem(Inventory, NewItem, Position, Size, Count);
+	}
+	if (Operation == "remove item" || Operation == "remove")
+	{
+		Item NewItem = DefineItem();
+		int Position = 0;
+		cout << "What object do you want to delete?";
+		do
+		{
+			cin >> Position;
+			if (!cin)
+			{
+				cerr << "Invalid Position must be within 0 and " << Size - 1;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+			}
+		} 
+		while (!cin);
+		RemoveItem(Inventory, Position, Size, Count);
+	}   
+	if (Operation == "output" || Operation == "print" || Operation == "<<")
+	{
+		Item NewItem = DefineItem();
+		string FileName;
+		ofstream OutputFile;
+		int Position = 0;
+		cout << "What output Filename do you prefer?";
+		do
+		{
+			cin >> FileName;
+			OutputFile.open(FileName, ios::out);
+			if (!OutputFile.is_open())
+			{
+				cerr << "Invalid Output Filename must be within 0 and " << Size - 1;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+			}
+		}
+		while (!cin);
+		RemoveItem(Inventory, Position, Size, Count);
+	}   
+	if (Operation == "search item" || Operation == "search" || Operation == "find")
+	{
+		Item NewItem = DefineItem();
+		string ItemName = "";
+		cout << "What object do you want to delete?";
+		do
+		{
+			cin >> ItemName;
+			if (!cin)
+			{
+				cerr << "Invalid ItemName must be a name!";
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+			}
+		}
+		while (!cin);
+		int Index = SearchForItem(Inventory, Size, ItemName);
+		if (Index == -1)
+		{
+			cout << "Item not found.";
+		}
+		else
+			cout << "Item found at " + Index;
+	}   
+}
+
+bool UserContinue() 
+{
+	bool Continue = false;
+	string Choice = "";
+	do
+	{
+		cout << "Do you want to continue y/n: ";
+		cin >> Choice;
+		if (!cin)
+		{
+			cerr << "Invalid Choice must be yes or no: ";
+			continue;
+		}
+		transform(Choice.begin(), Choice.end(), Choice.begin(), tolower); // Turn choice lowercase
+		if (Choice == "y" || Choice == "yes")
+		{
+			Continue = true;
+		}
+		else if (Choice == "no" || Choice == "n") 
+		{
+			Continue = false;
+			cout << endl << "Goodbye then.";
+		}
+	} while (!cin);
+	return Continue;
+}
 void CategorizeItems(ifstream& File, Item* List, int& Size, int& Count) 
 {
 	int ItemNumber;
--- Item.h
+++ Item.h
@@ -20,6 +20,8 @@ struct Item
 };
 static int DefaultSize = 0, DefaultCount = 0;
 void OpenInvenentoryFile(ifstream&);
+void ChooseOperation(ifstream&, Item*, int& Size, int& Count = DefaultCount);
+bool UserContinue();
 void CategorizeItems(ifstream&, Item*, int& Size, int& Count = DefaultCount);
 Item DefineItem();
 void AddItem(Item*&, Item&, int Position = 0, int& Size = DefaultSize, int& Count = DefaultCount);
