285781f2ce1ecca588e2adf6960f107e6badebcd
George Davis <davisgl055@ecc.edu>
2024-02-24 20:51:12 -0500

    Made main function better.

--- Iventory.cpp
+++ Iventory.cpp
@@ -13,7 +13,20 @@ int main()
 	cin >> Operation;
 	if (Operation == "add item" || Operation == "add")
 	{
-
+		Item NewItem = DefineItem();
+		int Position = 0;
+		cout << "What position do you want your new object to be in?";
+		while (!cin)
+		{
+			cin >> Position;
+			if (!cin)
+			{
+				cerr << "Invalid Position must be within 0 and " << Size - 1;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+			}
+		}
+		AddItem(Inventory, NewItem, Position, Size, Count);
 	}
 	return 0;
 }
